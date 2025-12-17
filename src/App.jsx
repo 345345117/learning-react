6c63539d3df4e69830f8906a2df82df868a30dda
singhdip12 <dipsingh654321@gmail.com>
2025-12-17 19:34:15 +0545

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,131 @@
+// function Apps(){
+//     let name="dip kumar singh"
+//     function fruits(a,b){
+//         return a+b;
+//     }
+//     function sum(){
+//         let y=20
+//         let x=30
+//         return x+y
+//     }
+//     function operation(a,b,c){
+//         if(a==0){
+//             return a
+//         } else if(b<10){
+//             return a+b
+//         }
+        
+//         else{
+//             return("This is the number")
+//         }
+//     }
+//     let arr={
+//         name:"shristi singh",
+//         Roll:20,
+//         subject:"nepali",
+//         village:"lahan",
+//         gemail:"www.gemail.com"
+        
+//     }
+//     let arr1=[1,2,3,34,4,4,5,5,5]
+//     let path="//img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"
+//    const path1="https://tse2.mm.bing.net/th/id/OIP.zMFqKDHhNFfp6lBVvRXlmAHaGP?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3"
+            
+        
+        
+    
+//     return(
+//         <div>
+//             <h1>{name}</h1>
+//             <p>{arr.gemail}</p>
+//             <h2>{sum()}</h2>
+//             <h3>{operation(2)}</h3>
+//             <h4>{arr1}</h4>
+//             <input type="text" value="Nature"></input>
+//             <img
+//             src={path}/>
+            
+            
+//             <img
+//             src={path1}/>
+            
+            
+
+//         </div>
+//     )
+// }
+// export default Apps
+// function Apps(){
+//     function name(b){
+//         alert("apple")
+//     }
+//     let fruits=(name)=>{
+//         alert(name)
+
+//     }
+    
+//     return(
+//         <div>
+//             <h1>Hwllo</h1>
+           
+//             <button onClick={()=>fruits("apple")}>summit2</button>
+
+
+//         </div>
+//     )
+// }
+// export default Apps
+// function Apps(){
+//     let fruits=(name)=>{
+//         alert(name)
+//     }
+
+//     return(
+//         <div>
+//             <h1>First page of my website</h1>
+//             <button onClick={()=>fruits("banana")}>sumit</button>
+//         </div>
+//     )
+// }
+// export default Apps
+// import { useState } from "react"
+// function Apple(){
+    
+//     let [fruits,setcount]=useState("apple")
+//     const name=()=>{
+//          setcount="Banana"
+//          console.log(fruits)
+//     }
+//     return(
+//         <div>
+//             <h1>This is my react website</h1>
+//             <h1>{fruits}</h1>
+//             <button onClick={()=>name()}>sumit</button>
+//         </div>
+//     )
+// }
+// export default Apple
+// import { useState } from "react"
+// import Name from "./component"
+
+// function Apps(){
+//     let [fruits,setCount]=useState("Apple")
+//     const call=()=>{
+//         setCount("banana")
+//     }
+
+//     return(
+//         <div>
+//             <Name/>
+        
+//             <h1>This is my first page</h1>
+//             <h1>{fruits}</h1>
+//             <button onClick={call}>summit</button>
+            
+            
+//         </div>
+//     )
+// }
+// export default Apps
+
+
---
+++ src/component.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react"
+
+// let  Name=()=>{
+//     let [initial,updatedcount]=useState(0)
+   
+      
+//     return(
+//         <div>
+//            <h1>update:{initial}</h1>
+//            <button onClick={()=>updatedcount(initial+1)}>up grade</button>
+
+
+//         </div>
+
+//     )
+// }
+// export default Name
